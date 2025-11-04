d3620234504056d3408d0f1326adc84557b45eae
AWTMedia <alexander.thomas2401@gmail.com>
2025-11-04 10:27:53 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 // src/App.tsx
-import React from "react";
-import { HashRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
-
-// Pages
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 
 export default function App() {
   return (
-    <HashRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Index />} />
+        {/* handle direct hits to /index.html */}
+        <Route path="/index.html" element={<Navigate to="/" replace />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
